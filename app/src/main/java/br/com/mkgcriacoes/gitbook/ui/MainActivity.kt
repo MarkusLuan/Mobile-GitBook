1e6608ee0f208de10a499b81da1525ac7154f36d
Markus Luan <markus23_luan@hotmail.com>
2021-10-12 14:17:58 -0300

    Adicionando o menu de pesquisa

--- app/src/main/java/br/com/mkgcriacoes/gitbook/ui/MainActivity.kt
+++ app/src/main/java/br/com/mkgcriacoes/gitbook/ui/MainActivity.kt
@@ -1,13 +1,20 @@
 package br.com.mkgcriacoes.gitbook.ui
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.inputmethod.InputMethodManager
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import br.com.mkgcriacoes.gitbook.R
 import br.com.mkgcriacoes.gitbook.adapters.RepositorioAdapter
 import br.com.mkgcriacoes.gitbook.databinding.ActivityMainBinding
 import br.com.mkgcriacoes.gitbook.viewModels.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     private val binding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -22,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
         binding.recvRepositorio.adapter = adapter
         viewModel.getRepositorios("MarkusLuan")
 
@@ -29,15 +38,39 @@ class MainActivity : AppCompatActivity() {
             .observe(this) {
                 when (it){
                     MainViewModel.State.Carregando -> {
-
+                        binding.progressbar.visibility = ProgressBar.VISIBLE
                     }
                     is MainViewModel.State.Erro -> {
-
+                        binding.progressbar.visibility = ProgressBar.GONE
+                        Toast.makeText(this, "Ocorreu um erro ao obter informações do github!", Toast.LENGTH_LONG).show()
                     }
                     is MainViewModel.State.Sucesso -> {
+                        binding.progressbar.visibility = ProgressBar.GONE
                         adapter.submitList(it.lista)
                     }
                 }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        println(newText)
+        return true
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        query?.let { viewModel.getRepositorios(it) }
+
+        (getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager)
+            .hideSoftInputFromWindow(binding.root.windowToken, 0)
+        return true
     }
 }
